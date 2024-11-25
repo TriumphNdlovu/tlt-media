7945f9bb8dce2458928ed2bb8e4273f7c50ff816
@Trevor240_ <realtriumphndlovu@gmail.com>
2024-11-25 04:36:42 +0200

    Refactor Navbar component to add text color to the navigation bar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -33,7 +33,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`fixed top-0 left-0 w-full z-50 py-4 px-6 transition-all  ${
+      className={`fixed top-0 left-0 w-full z-50 py-4 px-6 transition-all text-white ${
         isScrolled ? "bg-black text-white shadow-lg" : "bg-transparent"
       }`}
     >
